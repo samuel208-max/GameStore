6d2e04d537d22300798c799a9e82e3a2413f565a
SAMUEL FERREIRA TRINDADE COSTA <samuel.ftcosta@senacsp.edu.br>
2026-02-03 11:49:40 -0300

    Inicio do desenvolvimento do carrinho com JS

--- public/index.php
+++ public/index.php
@@ -208,4 +208,18 @@ include '../includes/header.php';
     </section>
 </main>
 
+<aside class="carrinho">
+    <div class="topoCarrinho">
+        <h2 class="tituloCarrinho">Carrinho</h2>
+        <button class="fecharCarrinho" id="fecharCarrinho">❌</button>
+    </div>
+
+    <ul class="produtosCarrinho" id="produtosCarrinho"></ul>
+
+    <div class="resumoFinalCarrinho">
+        <p>Total: <span id="totalCarrinho">R$ 00,00</span></p>
+        <button class="btnFinalizarCompra">Finalizar Compra</button>
+    </div>
+</aside>
+
 <?php include '../includes/footer.php'; ?>
